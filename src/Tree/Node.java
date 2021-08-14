2671d55c6fc099bda1a51fc0c3d074b9d82bb71d
Omar <omcoello@espol.edu.ec>
2021-08-14 14:39:05 -0500

    Definir arbol generico

--- src/Tree/Node.java
+++ src/Tree/Node.java
@@ -5,11 +5,39 @@
  */
 package Tree;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author omarc
  * @param <T>
  */
 public class Node<T> {
+    private T content;
+    private ArrayList<Tree<T>> children;
     
+    public Node(){
+        this.children = new ArrayList<>();
+    }
+    
+    public Node(T content){
+        this.content = content;
+        this.children = new ArrayList<>();
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public ArrayList<Tree<T>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<Tree<T>> children) {
+        this.children = children;
+    }
 }
--- src/Tree/Tree.java
+++ src/Tree/Tree.java
@@ -13,6 +13,22 @@ package Tree;
  */
 public class Tree<T>  {
     
+    private Node<T> root;
     
+    public Tree() {
+        this.root = new Node<>();
+    }
+
+    public Tree(T content) {
+        this.root = new Node<>(content);
+    }
+
+    public Node<T> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node<T> root) {
+        this.root = root;
+    }
     
 }
